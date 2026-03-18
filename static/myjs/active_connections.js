a803635b7eb81b3a21cb05a6a55d2d4d467bb517
Wangchuk <chukong5@gmail.com>
2026-03-18 13:29:55 +1100

    Fixed the error in active_connections.js file of setting value of nul element. And Released

--- static/myjs/active_connections.js
+++ static/myjs/active_connections.js
@@ -5,30 +5,32 @@ let socket = io();
 
 // Listen for the 'update_client_list' event from the server
 socket.on('update_client_list', (clients) => {
-    document.getElementById('count').textContent = Object.keys(clients).length;
+    let countsDiv = document.getElementById('count');
     let clientListDiv = document.getElementById('client_list_div');
-    clientListDiv.innerHTML = '';
+    if (clientListDiv !== null && countsDiv !== null) {
+        clientListDiv.innerHTML = '';
+        countsDiv.textContent = Object.keys(clients).length;
 
-
-    for (const [key, value] of Object.entries(clients)) {
-        let clientInfoHTML = `<div class="col-sm-2  text-right">SID :</div>
+        for (const [key, value] of Object.entries(clients)) {
+            let clientInfoHTML = `<div class="col-sm-2  text-right">SID :</div>
                                 <div class="col-sm-10  px-0">${key}</div>`
 
-        for (const [itemkey, itemValue] of Object.entries(value)) {
-            clientInfoHTML += `<div class="col-sm-2  text-right">${itemkey} :</div>
+            for (const [itemkey, itemValue] of Object.entries(value)) {
+                clientInfoHTML += `<div class="col-sm-2  text-right">${itemkey} :</div>
                                 <div class="col-sm-10  px-0">${itemValue}</div>`
-        }
+            }
 
-        var mainHTML = `<div class="row py-2 bg-secondary text-light mb-1">
+            var mainHTML = `<div class="row py-2 bg-secondary text-light mb-1">
                             ${clientInfoHTML}
                       </div>`
 
-        clientListDiv.insertAdjacentHTML('beforeend', mainHTML);
+            clientListDiv.insertAdjacentHTML('beforeend', mainHTML);
+        }
 
+        if (countsDiv !== null && clients !== null) {
+            
+        }
     }
-
-
-
 }
 );
 
