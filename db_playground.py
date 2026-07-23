e6b3f16db008ca8ab29b8a28abb8fdd42bfcb5be
Wangchuk <chukong5@gmail.com>
2026-07-23 13:29:50 +1000

    Added db_playground.py to test databae functions

---
+++ db_playground.py
@@ -0,0 +1,26 @@
+# Wangchuk added 23-07-2026
+# To test database functionality
+
+from manager_quote_builder_db import core
+from manager_quote_builder_db import frequently_used_texts
+from manager_quote_builder_db import section_image_size
+from manager_quote_builder_db import user_types
+from manager_quote_builder_db import users
+from manager_quote_builder_db import quote_status
+from manager_quote_builder_db import quotes
+from manager_quote_builder_db import eo_excel_text
+from manager_quote_builder_db import eo_excel
+from manager_quote_builder_db import image_tags
+from manager_quote_builder_db import images
+from manager_quote_builder_db import texts
+from manager_quote_builder_db import section_names
+from manager_quote_builder_db import quote_details
+from manager_quote_builder_db import quote_section_info
+from manager_quote_builder_db import clickup_clients
+from manager_quote_builder_db import quote_composite
+
+if __name__ == "__main__":
+    print()
+
+
+    pass
